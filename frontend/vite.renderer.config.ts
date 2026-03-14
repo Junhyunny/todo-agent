3760110e1a797b95fb5844f9ffbee33464616566
Junhyunny <kang3966@naver.com>
2026-03-14 23:20:03 +0900

    [#200018461] frontend test environment setup

--- frontend/vite.renderer.config.ts
+++ frontend/vite.renderer.config.ts
@@ -1,4 +1,11 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import {defineConfig} from "vite";
 
-// https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        environment: "jsdom",
+        setupFiles: ["./vitest-setup.ts"],
+    },
+});
