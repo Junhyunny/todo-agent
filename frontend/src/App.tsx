a163037d026d4d43fe9b3db07300a4b2fa7d7b31
Junhyunny <kang3966@naver.com>
2026-04-03 22:57:06 +0900

    [#200019457] tailwind v4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 // biome-ignore lint/correctness/noUnusedImports: need for proper rendering
 import React from "react";
 
-export const App = () => <h1>Hello Electron + React World</h1>;
+export const App = () => (
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+);
--- frontend/vite.renderer.config.ts
+++ frontend/vite.renderer.config.ts
@@ -1,9 +1,10 @@
-/// <reference types="vitest" />
+// biome-ignore assist/source/organizeImports: need for proper rendering
 import react from "@vitejs/plugin-react";
-import {defineConfig} from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import {defineConfig} from "vitest/config";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react({}), tailwindcss(),],
     test: {
         environment: "jsdom",
         setupFiles: ["./vitest-setup.ts"],
