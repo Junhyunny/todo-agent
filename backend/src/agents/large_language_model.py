91cf4449818dd6dbf5538536a81596b373f6a578
Junhyunny <kang3966@naver.com>
2026-04-22 23:07:13 +0900

    [no-pt-story] management language model name with an array

--- backend/src/agents/large_language_model.py
+++ backend/src/agents/large_language_model.py
@@ -2,11 +2,17 @@ from dotenv import load_dotenv
 from langchain_aws import ChatBedrockConverse
 from langchain_core.language_models import BaseChatModel
 
+models = [
+  "global.anthropic.claude-sonnet-4-6",
+  "global.anthropic.claude-haiku-4-5-20251001-v1:0",
+  "apac.amazon.nova-pro-v1:0",
+]
+
 
 def get_llm() -> BaseChatModel:
   load_dotenv()
   return ChatBedrockConverse(
-    model="apac.amazon.nova-pro-v1:0",
+    model=models[1],
     temperature=1,
     region_name="ap-northeast-1",
   )
